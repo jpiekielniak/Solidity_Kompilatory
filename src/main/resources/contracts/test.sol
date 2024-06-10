fea5ac713b11090ec74b95030cc2768dd3ebe88a
Justyna <justyna.kurcz1@gmail.com>
2024-06-10 16:53:00 +0200

    update: test.sol

--- src/main/resources/contracts/test.sol
+++ src/main/resources/contracts/test.sol
@@ -4,14 +4,8 @@ pragma solidity ^0.8.0;
 contract SimpleStorage {
     uint256 private storedData;
 
-    event DataStored(uint256 data);
-
-    function set(uint256 x) public {
+    constructor(uint256 x) {
         storedData = x;
-        emit DataStored(x);
     }
 
-    function get() public view returns (uint256) {
-        return storedData;
-    }
 }
